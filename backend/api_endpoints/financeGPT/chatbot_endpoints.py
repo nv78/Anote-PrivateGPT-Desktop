ef25e58bf82460bd58cc57603799f481faa0125a
eden-chung <edenchung23@gmail.com>
2024-02-06 01:28:24 -0500

    more functions

--- backend/api_endpoints/financeGPT/chatbot_endpoints.py
+++ backend/api_endpoints/financeGPT/chatbot_endpoints.py
@@ -37,6 +37,22 @@ def add_chat_to_db(chat_type, model_type): #intake the current userID and the mo
 
     return chat_id
 
+def update_chat_name_db(chat_id, new_name):
+    conn, cursor = get_db_connection()
+
+    query = """
+    UPDATE chats
+    JOIN users ON chats.user_id = users.id
+    SET chats.chat_name = ?
+    WHERE chats.id = ? AND users.id = ?;
+    """
+    cursor.execute(query, (new_name, chat_id, USER_ID))
+
+    conn.commit()
+    conn.close()
+
+    return
+
 def retrieve_chats_from_db():
     conn, cursor = get_db_connection()
 
@@ -77,6 +93,88 @@ def retrieve_message_from_db(chat_id, chat_type):
 
     return messages
 
+
+def delete_chat_from_db(chat_id):
+    print("delete chat from db")
+    conn, cursor = get_db_connection()
+
+    delete_chunks_query = """
+    DELETE chunks
+    FROM chunks
+    INNER JOIN documents ON chunks.document_id = documents.id
+    INNER JOIN chats ON documents.chat_id = chats.id
+    INNER JOIN users ON chats.user_id = users.id
+    WHERE chats.id = ? AND users.id = ?;
+    """
+    cursor.execute(delete_chunks_query, (chat_id, USER_ID))
+
+    delete_documents_query = """
+    DELETE documents
+    FROM documents
+    INNER JOIN chats ON documents.chat_id = chats.id
+    INNER JOIN users ON chats.user_id = users.id
+    WHERE chats.id = ? AND users.id = ?;
+    """
+    cursor.execute(delete_documents_query, (chat_id, USER_ID))
+
+    delete_messages_query = """
+    DELETE messages
+    FROM messages
+    INNER JOIN chats ON messages.chat_id = chats.id
+    INNER JOIN users ON chats.user_id = users.id
+    WHERE chats.id = ? AND users.id = ?;
+    """
+    cursor.execute(delete_messages_query, (chat_id, USER_ID))
+
+    query = """
+    DELETE chats
+    FROM chats
+    INNER JOIN users ON chats.user_id = users.id
+    WHERE chats.id = ? AND users.id = ?;
+    """
+    cursor.execute(query, (chat_id, USER_ID))
+
+    conn.commit()
+
+    if cursor.rowcount > 0:
+        print(f"Deleted chat with ID {chat_id} for user {USER_ID}.")
+        conn.close()
+        cursor.close()
+        return 'Successfully deleted'
+    else:
+        print(f"No chat deleted. Chat ID {chat_id} may not exist or does not belong to user {USER_ID}.")
+        conn.close()
+        cursor.close()
+        return 'Could not delete'
+
+
+def reset_chat_db(chat_id):
+    print("reset chat")
+    conn, cursor = get_db_connection()
+
+    delete_messages_query = """
+    DELETE messages
+    FROM messages
+    INNER JOIN chats ON messages.chat_id = chats.id
+    INNER JOIN users ON chats.user_id = users.id
+    WHERE chats.id = ? AND users.id = ?;
+    """
+    cursor.execute(delete_messages_query, (chat_id, USER_ID))
+
+    conn.commit()
+
+    if cursor.rowcount > 0:
+        print(f"Deleted chat with ID {chat_id} for user {USER_ID}.")
+        conn.close()
+        cursor.close()
+        return 'Successfully deleted'
+    else:
+        print(f"No chat deleted. Chat ID {chat_id} may not exist or does not belong to user {USER_ID}.")
+        conn.close()
+        cursor.close()
+        return 'Could not delete'
+    
+
 def find_most_recent_chat_from_db():
     conn, cursor = get_db_connection()
 
@@ -97,6 +195,22 @@ def find_most_recent_chat_from_db():
     conn.close()
 
     return chat_info
+
+def change_chat_mode_db(chat_mode_to_change_to, chat_id):
+    conn, cursor = get_db_connection()
+
+    query = """
+    UPDATE chats
+    JOIN users ON chats.user_id = users.id
+    SET chats.model_type = ?
+    WHERE chats.id = ? AND users.id = ?;
+    """
+    
+    # Execute the query
+    cursor.execute(query, (chat_mode_to_change_to, chat_id, USER_ID))
+
+    conn.commit()
+    conn.close()
 
 def add_document_to_db(text, document_name, chat_id):
     conn, cursor = get_db_connection()
@@ -152,6 +266,106 @@ def chunk_document(text, maxChunkSize, document_id):
     conn.close()
 
 
+def knn(x, y):
+    x = np.expand_dims(x, axis=0)
+    # Calculate cosine similarity
+    similarities = np.dot(x, y.T) / (np.linalg.norm(x) * np.linalg.norm(y))
+    # Convert similarities to distances
+    distances = 1 - similarities.flatten()
+    nearest_neighbors = np.argsort(distances)
+
+    results = []
+    for i in range(len(nearest_neighbors)):
+        item = {
+            "index": nearest_neighbors[i],
+            "similarity_score": distances[nearest_neighbors[i]]
+        }
+        results.append(item)
+
+    return results
+
+def get_relevant_chunks(k, question, chat_id):
+    conn, cursor = get_db_connection()
+
+    query = """
+    SELECT c.start_index, c.end_index, c.embedding_vector, c.document_id, c.page_number, d.document_name
+    FROM chunks c
+    JOIN documents d ON c.document_id = d.id
+    JOIN chats ch ON d.chat_id = ch.id
+    JOIN users u ON ch.user_id = u.id
+    WHERE u.id = ? AND ch.id = ?
+    """
+
+    cursor.execute(query, (USER_ID, chat_id))
+    rows = cursor.fetchall()
+
+    embeddings = []
+    for row in rows:
+        embeddingVectorBlob = row["embedding_vector"]
+        embeddingVector = np.frombuffer(embeddingVectorBlob)
+        embeddings.append(embeddingVector)
+
+    if (len(embeddings) == 0):
+        res_list = []
+        for i in range(k):
+            res_list.append("No text found")
+        return res_list
+
+    embeddings = np.array(embeddings)
+
+    embeddingVector = openai.embeddings.create(input=question, model="text-embedding-ada-002").data[0].embedding
+    embeddingVector = np.array(embeddingVector)
+
+    res = knn(embeddingVector, embeddings)
+
+    #Get the k most relevant chunks
+    source_chunks = []
+    for i in range(k):
+        source_id = res[i]['index']
+
+        document_id = rows[source_id]['document_id']
+        page_number = rows[source_id]['page_number']
+        document_name = rows[source_id]['document_name']
+
+
+        cursor.execute('SELECT document_text FROM documents WHERE id = ?', (document_id,))
+        doc_text = cursor.fetchone()['document_text']
+
+        source_chunk = doc_text[rows[source_id]['start_index']:rows[source_id]['end_index']]
+        source_chunks.append((source_chunk, page_number, document_name))
+        #source_chunks.append(source_chunk)
+
+    return source_chunks
+
+def add_sources_to_db(message_id, sources):
+    combined_sources = ""
+
+    for source in sources:
+        chunk_text, page_number, document_name = source
+        combined_sources += f"Document: {document_name}, Page {page_number}: {chunk_text}\n\n"
+
+    conn, cursor = get_db_connection()
+
+    cursor.execute('UPDATE messages SET relevant_chunks = ? WHERE id = ?', (combined_sources, message_id))
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+def add_message_to_db(text, chat_id, isUser):
+    #If isUser is 0, it is a bot message, 1 is a user message
+    conn, cursor = get_db_connection()
+
+    cursor.execute('INSERT INTO messages (message_text, chat_id, sent_from_user) VALUES (?,?,?)', (text, chat_id, isUser))
+    message_id = cursor.lastrowid
+
+    conn.commit()
+    conn.close()
+
+    return message_id
+
+
 def retrieve_docs_from_db(chat_id):
     conn, cursor = get_db_connection()
 
@@ -200,3 +414,17 @@ def delete_doc_from_db(doc_id):
     conn.close()
 
     return "success"
+
+def add_model_key_to_db(model_key, chat_id, user_email):
+    conn, cursor = get_db_connection()
+
+    update_query = """
+        UPDATE chats
+        JOIN users ON chats.user_id = users.id
+        SET chats.custom_model_key = ?
+        WHERE chats.id = ? AND users.email = ?;
+        """
+
+    cursor.execute(update_query, (model_key, chat_id, USER_ID))
+
+    conn.commit()
--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,14 @@ from flask import Flask, request, jsonify, abort, redirect, send_file
 from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
 from tika import parser as p
+import openai
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+import os
+
 
 from api_endpoints.financeGPT.chatbot_endpoints import add_chat_to_db, retrieve_chats_from_db, retrieve_message_from_db, retrieve_docs_from_db, delete_doc_from_db, \
-                                                        find_most_recent_chat_from_db, add_document_to_db, chunk_document
+                                                        find_most_recent_chat_from_db, add_document_to_db, chunk_document, update_chat_name_db, delete_chat_from_db, \
+                                                        reset_chat_db, change_chat_mode_db, add_message_to_db, get_relevant_chunks, add_sources_to_db, add_model_key_to_db
 
 
 
@@ -58,6 +63,21 @@ def retrieve_messages_from_chat():
 
     return jsonify(messages=messages)
 
+@app.route('/update-chat-name', methods=['POST'])
+def update_chat_name():
+
+    chat_name = request.json.get('chat_name')
+    chat_id = request.json.get('chat_id')
+
+    update_chat_name_db(chat_id, chat_name)
+
+    return "Chat name updated"
+
+@app.route('/delete-chat', methods=['POST'])
+def delete_chat():
+    chat_id = request.json.get('chat_id')
+
+    return delete_chat_from_db(chat_id)
 
 @app.route('/find-most-recent-chat', methods=['POST'])
 def find_most_recent_chat():
@@ -105,6 +125,110 @@ def delete_doc():
     delete_doc_from_db(doc_id)
 
     return "success"
+
+@app.route('/change-chat-mode', methods=['POST'])
+def change_chat_mode_and_reset_chat():
+    chat_mode_to_change_to = request.json.get('model_type')
+    chat_id = request.json.get('chat_id')
+
+    try:
+        reset_chat_db(chat_id)
+        change_chat_mode_db(chat_mode_to_change_to, chat_id)
+
+        return "Success"
+    except:
+        return "Error"
+
+@app.route('/reset-chat', methods=['POST'])
+def reset_chat():
+    chat_id = request.json.get('chat_id')
+
+    return reset_chat_db(chat_id)
+
+
+@app.route('/process-message-pdf', methods=['POST'])
+def process_message_pdf():
+    message = request.json.get('message')
+    chat_id = request.json.get('chat_id')
+    model_type = request.json.get('model_type')
+    model_key = request.json.get('model_key')
+
+    ##Include part where we verify if user actually owns the chat_id later
+
+    query = message.strip()
+
+    #This adds user message to db
+    add_message_to_db(query, chat_id, 1)
+
+    #Get most relevant section from the document
+    sources = get_relevant_chunks(2, query, chat_id)
+
+    if (model_type == 0):
+        if model_key:
+           model_use = model_key
+        else:
+           model_use = "gpt-4"
+
+        print("using OpenAI and model is", model_use)
+        client = openai.OpenAI()
+        try:
+            completion = client.chat.completions.create(
+                model=model_use,
+                messages=[
+                    {"role": "user",
+                     "content": f"You are a factual chatbot that answers questions about 10-K documents. You only answer with answers you find in the text, no outside information. These are the sources from the text:{sources[0]}{sources[1]} And this is the question:{query}."}
+                ]
+            )
+            print("using fine tuned model")
+            answer = str(completion.choices[0].message.content)
+        except openai.NotFoundError:
+            print(f"The model `{model_use}` does not exist. Falling back to 'gpt-4'.")
+            completion = client.chat.completions.create(
+                model="gpt-4",
+                messages=[
+                    {"role": "user",
+                     "content": f"First, tell the user that their given model key does not exist, and that you have resorted to using GPT-4 before answering their question, then add a line break and answer their question. You are a factual chatbot that answers questions about 10-K documents. You only answer with answers you find in the text, no outside information. These are the sources from the text:{sources[0]}{sources[1]} And this is the question:{query}."}
+                ]
+            )
+            answer = str(completion.choices[0].message.content)
+    else:
+        print("using Claude")
+
+        anthropic = Anthropic(
+            api_key=os.environ.get("ANTHROPIC_API_KEY")
+        )
+
+        completion = anthropic.completions.create(
+            model="claude-2",
+            max_tokens_to_sample=700,
+            prompt = (
+              f"{HUMAN_PROMPT} "
+              f"You are a factual chatbot that answers questions about 10-K documents. You only answer with answers you find in the text, no outside information. "
+              f"please address the question: {query}. "
+              f"Consider the provided text as evidence: {sources[0]}{sources[1]}. "
+              f"{AI_PROMPT}")
+        )
+        answer = completion.completion
+
+    #This adds bot message
+    message_id = add_message_to_db(answer, chat_id, 0)
+    
+    try:
+        add_sources_to_db(message_id, sources)
+    except:
+        print("no sources")
+
+    return jsonify(answer=answer)
+
+@app.route('/add-model-key', methods=['POST'])
+def add_model_key():
+    model_key = request.json.get('model_key')
+    chat_id = request.json.get('chat_id')
+
+    add_model_key_to_db(model_key, chat_id)
+
+    return "success"
+
 
 if __name__ == '__main__':
     app.run(port=5000)
