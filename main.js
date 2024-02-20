b1d4100cad0e2d476c31a68987959335310a36a0
eden-chung <edenchung23@gmail.com>
2024-02-19 21:30:16 -0500

    modify package to create dmg

--- main.js
+++ main.js
@@ -95,7 +95,18 @@ function createWindow() {
 
 
 
-app.whenReady().then(createWindow);
+/* app.whenReady().then(createWindow); */
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
 
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
